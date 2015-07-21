fef59301469d74fd710abc95d3dd6e2d46f4c9a9
Stephen Cui <yscui@thoughtworks.com>
2015-07-21 23:04:57 +0800

    could scrapy some data

--- zhidao/zhidao/spiders/zhidao_spider.py
+++ zhidao/zhidao/spiders/zhidao_spider.py
@@ -12,18 +12,19 @@ except:
 from scrapy.utils.response import get_base_url
 from scrapy.utils.url import urljoin_rfc
 from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor as sle
+from scrapy.linkextractors.sgml import SgmlLinkExtractor as sle
 
 class ZhidaoSpider(CrawlSpider):
     name = "zhidao"
     allowed_domains = ["baidu.com"]
     start_urls = [
-        #"http://zhidao.baidu.com"
-        "http://zhidao.baidu.com/question/491451027.html"
+        "http://zhidao.baidu.com"
+        #"http://zhidao.baidu.com/question/491451027.html"
     ]
     rules = [
-        #Rule(sle(allow=("/question/\d+\.html")), follow=True, callback='parse_item')
-        Rule(sle(allow=("/question/491451027.html")), callback='parse_item')
+        #Rule(sle(allow=(r'/question/\d+\.html')), follow=True, callback='parse_log'),
+        Rule(sle(allow=(r'/question/\d+\.html')), follow=True, callback='parse_item')
+        #Rule(sle(allow=("/question/491451027.html")), callback='parse_item')
     ]
 
     def parse_item(self, response):
@@ -32,10 +33,10 @@ class ZhidaoSpider(CrawlSpider):
         base_url = get_base_url(response)
 
         item = ZhidaoItem()
-        # item['address'] = base_url
-        # item['question'] = sel.css('#wgt-ask > h1 > span::text').extract()
-        # item['questionDetail'] = sel.css('#wgt-ask > pre:not(#selectsearch-icon)::text').extract()
-        # item['answerDetail'] = sel.css('.line .content > pre::text').extract()
+        item['address'] = base_url
+        item['question'] = sel.css('#wgt-ask > h1 > span::text').extract()
+        item['questionDetail'] = sel.css('#wgt-ask > pre:not(#selectsearch-icon)::text').extract()
+        item['answerDetail'] = sel.css('.line .content > pre::text').extract()
         item['praiseNumber'] = sel.css('div.grid-r.f-aid.mt-15 .evaluate-num-fixed::text').extract_first(default='0')
         item['answerDate'] = sel.css('span.grid-r.f-aid.pos-time.mt-15').xpath('text()').extract()
         items.append(item)
@@ -43,7 +44,11 @@ class ZhidaoSpider(CrawlSpider):
         info('DEBUG--> ' + str(item['praiseNumber']))
         info('parsed ' + str(response))
         return items
-   
+
+    def parse_log(self, response):
+        info('parsed ' + str(response))
+        return []
+
     def close_spider(self):
         self.close(CrawlSpider,"Manual Stop...")
 
