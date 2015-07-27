fdb75186d4e3876b53b6c3920d938f3b471884b4
Stephen Cui <yscui@thoughtworks.com>
2015-07-27 23:33:46 +0800

    Update answerDate css selector

--- zhidao/zhidao/spiders/zhidao_spider.py
+++ zhidao/zhidao/spiders/zhidao_spider.py
@@ -14,6 +14,8 @@ from scrapy.utils.url import urljoin_rfc
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.linkextractors.sgml import SgmlLinkExtractor as sle
 
+from datetime import *
+
 class ZhidaoSpider(CrawlSpider):
     name = "zhidao"
     allowed_domains = ["baidu.com"]
@@ -26,19 +28,26 @@ class ZhidaoSpider(CrawlSpider):
     ]
 
     def parse_item(self, response):
+        dt = datetime.now()
+        dt_default = "\n" + dt.strftime("%Y-%m-%d %H:%M") + "\n"
         items = []
         sel = Selector(response)
         base_url = get_base_url(response)
 
         item = ZhidaoItem()
         item['address'] = base_url
-        item['question'] = sel.xpath('//div[@id="wgt-ask"]/h1/span/text()').extract_first(default='')
-        item['questionDetail'] = sel.xpath('//*[@id="wgt-ask"]/pre/text()').extract_first(default='')
-        item['answerDetail'] = sel.css('.line .content > pre::text').extract_first(default='')
-        item['praiseNumber'] = sel.css('.grid-r mt-15 .span:nth-child(1) .b:nth-child(2)::text').extract_first(default='0')
-        item['answerDate'] = sel.css('span.grid-r.f-aid.pos-time.mt-15').xpath('text()').extract_first(default='20150726')
-        items.append(item)
+        # item['question'] = sel.xpath('//div[@id="wgt-ask"]/h1/span/text()').extract_first(default='')
+        # item['questionDetail'] = sel.xpath('//*[@id="wgt-ask"]/pre/text()').extract_first(default='')
+        # item['answerDetail'] = sel.css('.line .content > pre::text').extract_first(default='')
+        # item['praiseNumber'] = sel.css('div.grid-r.f-aid.mt-15 .evaluate-num-fixed::text').extract()
 
+        # Node: answerDate
+        if(len(sel.css('span.grid-r.f-aid.pos-time::text').extract()) > 1):
+            item['answerDate'] = sel.css('span.grid-r.f-aid.pos-time::text').extract()[1]
+        else:
+            item['answerDate'] = sel.css('span.grid-r.f-aid.pos-time::text').extract_first(default=dt_default)
+
+        items.append(item)
         info('parsed ' + str(response))
         return items
 
